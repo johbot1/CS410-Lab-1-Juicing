3823625bb05cf397e229cfe06400f7f35b244960
John <jbotonakis@carroll.edu>
2025-01-31 14:54:48 -0700

    Replaced Plant with single threaded Plant

--- src/Plant.java
+++ src/Plant.java
@@ -1,39 +1,80 @@
-public class Plant {
+public class Plant implements Runnable {
     // How long do we want to run the juice processing
     public static final long PROCESSING_TIME = 5 * 1000;
 
     public static void main(String[] args) {
         // Startup a single plant
-        Plant p = new Plant();
+        final Plant p = new Plant(0);
+        p.startPlant();
 
         // Give the plants time to do work
-        long endTime = System.currentTimeMillis() + PROCESSING_TIME;
-        int provided = 0;
-        while (System.currentTimeMillis() < endTime) {
-            p.processEntireOrange(new Orange());
-            provided++;
-        }
+        delay(PROCESSING_TIME, "Plant malfunction");
+
+        // Stop the plant, and wait for it to shutdown
+        p.stopPlant();
 
         // Summarize the results
-        System.out.println("Total provided/processed = " + provided + "/" + p.getProcessedOranges());
+        System.out.println("Total provided/processed = " + p.getProvidedOranges() + "/" + p.getProcessedOranges());
         System.out.println("Created " + p.getBottles() +
                 ", wasted " + p.getWaste() + " oranges");
     }
 
-    public final int ORANGES_PER_BOTTLE = 4;
+    private static void delay(long time, String errMsg) {
+        long sleepTime = Math.max(1, time);
+        try {
+            Thread.sleep(sleepTime);
+        } catch (InterruptedException e) {
+            System.err.println(errMsg);
+        }
+    }
 
+    public final int ORANGES_PER_BOTTLE = 3;
+
+    private final Thread thread;
+    private int orangesProvided;
     private int orangesProcessed;
+    private volatile boolean timeToWork;
 
-    Plant() {
+    Plant(int threadNum) {
+        orangesProvided = 0;
         orangesProcessed = 0;
+        thread = new Thread(this, "Plant[" + threadNum + "]");
+    }
+
+    public void startPlant() {
+        timeToWork = true;
+        thread.start();
+    }
+
+    public void stopPlant() {
+        timeToWork = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            System.err.println(thread.getName() + " stop malfunction");
+        }
+    }
+
+    public void run() {
+        System.out.print(Thread.currentThread().getName() + " Processing oranges");
+        while (timeToWork) {
+            processEntireOrange(new Orange());
+            orangesProvided++;
+            System.out.print(".");
+        }
+        System.out.println("");
+        System.out.println(Thread.currentThread().getName() + " Done");
     }
 
     public void processEntireOrange(Orange o) {
         while (o.getState() != Orange.State.Bottled) {
             o.runProcess();
-            // o.nextState();
         }
         orangesProcessed++;
+    }
+
+    public int getProvidedOranges() {
+        return orangesProvided;
     }
 
     public int getProcessedOranges() {
