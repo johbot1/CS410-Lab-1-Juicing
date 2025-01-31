955817c8bd1b003a96ceefc1ec933ded12bb621b
John <jbotonakis@carroll.edu>
2025-01-31 14:57:21 -0700

    Replaced Plant with multi-threaded Plant

--- src/Plant.java
+++ src/Plant.java
@@ -2,21 +2,41 @@ public class Plant implements Runnable {
     // How long do we want to run the juice processing
     public static final long PROCESSING_TIME = 5 * 1000;
 
+    private static final int NUM_PLANTS = 2;
+
     public static void main(String[] args) {
-        // Startup a single plant
-        final Plant p = new Plant(0);
-        p.startPlant();
+        // Startup the plants
+        Plant[] plants = new Plant[NUM_PLANTS];
+        for (int i = 0; i < NUM_PLANTS; i++) {
+            plants[i] = new Plant(1);
+            plants[i].startPlant();
+        }
 
         // Give the plants time to do work
         delay(PROCESSING_TIME, "Plant malfunction");
 
         // Stop the plant, and wait for it to shutdown
-        p.stopPlant();
+        for (Plant p : plants) {
+            p.stopPlant();
+        }
+        for (Plant p : plants) {
+            p.waitToStop();
+        }
 
         // Summarize the results
-        System.out.println("Total provided/processed = " + p.getProvidedOranges() + "/" + p.getProcessedOranges());
-        System.out.println("Created " + p.getBottles() +
-                ", wasted " + p.getWaste() + " oranges");
+        int totalProvided = 0;
+        int totalProcessed = 0;
+        int totalBottles = 0;
+        int totalWasted = 0;
+        for (Plant p : plants) {
+            totalProvided += p.getProvidedOranges();
+            totalProcessed += p.getProcessedOranges();
+            totalBottles += p.getBottles();
+            totalWasted += p.getWaste();
+        }
+        System.out.println("Total provided/processed = " + totalProvided + "/" + totalProcessed);
+        System.out.println("Created " + totalBottles +
+                ", wasted " + totalWasted + " oranges");
     }
 
     private static void delay(long time, String errMsg) {
@@ -48,6 +68,9 @@ public class Plant implements Runnable {
 
     public void stopPlant() {
         timeToWork = false;
+    }
+
+    public void waitToStop() {
         try {
             thread.join();
         } catch (InterruptedException e) {
