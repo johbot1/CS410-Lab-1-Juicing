d7a2d78443ac55957e80a72356110d2a77f88213
John <jbotonakis@carroll.edu>
2025-02-04 16:44:02 -0700

    Finished Mutex and added ALOT of comments

--- src/Mutex.java
+++ src/Mutex.java
@@ -1,9 +1,55 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+//This ensures mutual exclusion meaning
+//only one thread at a time can access a critical section.
 public class Mutex {
-    public boolean acquire() {
+    //Keeps track of state of locked (true) or unlocked (false)
+    private boolean isLocked = false;
+
+    //First in First Out Waiting Line
+    private final Queue<Thread> waitingThreads = new LinkedList<>();
+
+    //Synchronized ensures only one thread can modify waitingQueue or isLocked
+    //This function will block a thread until it's allowed the Mutex
+    public synchronized boolean acquire() {
+        //Gets the current thread, then adds it to the queue
+        //to know who is waiting. This ensures first come,
+        //first served fairness.
+        Thread currentThread = Thread.currentThread();
+        waitingThreads.add(currentThread);
+
+        //Wait if currently locked, meaning another thread has the mutex
+        //or if the current thread is not first in line
+        while(isLocked || waitingThreads.peek() != currentThread){
+            //The thread pauses execution and then releases the lock.
+            //If it's interrupted, the thread restores it's interrupted
+            //state, ensuring consistency/context preservation
+            try{
+                wait();
+            } catch(InterruptedException e){
+                Thread.currentThread().interrupt(); //Preserve interrupt status
+            }
+        }
+        //On exit, the mutex is now free, and the current thread
+        //is the first in line in the queue.
+        //Remove the current thread from the queue since it don't need
+        //to wait anymore, then lock the mutex back up.
+        waitingThreads.poll();
+        isLocked= true;
         return true;
     }
 
-    public void release() {
-        return;
+    //Unlocks the Mutex, synchronized ensures only one Thread
+    //can use the Mutex at a time.
+    public synchronized void release() {
+        //Unlocks the mutex, waiting for the next thread
+        isLocked = false;
+        //If there are any threads in the waiting queue,
+        //wake them up. I use notifyAll because I don't know
+        //which thread specifically will be the next to run
+        if (!waitingThreads.isEmpty()) {
+            notifyAll();
+        }
     }
 }
--- src/MutexTestSpinLoop.java
+++ src/MutexTestSpinLoop.java
@@ -1,7 +1,18 @@
 public class MutexTestSpinLoop implements Runnable {
+    //TL;DR: The main thread starts 5 new threads (T[0],T[1],...T[4]
+    //Each thread prints running, but doesn't necessarily execute in order
+    //Only one thread can enter the 'acquire' section at a time
+    //Thread prints "Acquired", waits 1 second, then releases the mutex
+    //The next thread in the queue wakes up and repeats the process.
+
+
+    //Initializes 5 threads
     private static final int NUM_THREADS = 5;
-    //USE synchronize, monitor
-    //Make sure acquire comes before release
+
+    //Creates a mutex instance every thread will share,
+    //Then creates 5 child threads, each running a SpinLoop instance.
+    //Order of execution is NOT gauranteed but all threads will
+    //eventually try to get time with the Mutex.
     public static void main(String[] args) {
         Mutex m = new Mutex();
 
@@ -10,30 +21,43 @@ public class MutexTestSpinLoop implements Runnable {
         }
     }
 
+    //Stores the mutex so everythread uses the same lock.
     private final Mutex m;
-
     MutexTestSpinLoop(Mutex m, int num) {
         this.m = m;
+        //Creates then immidiately starts a new thread
+        //Threads will not necessarily start in order
+        //This is due to the OS scheduling
         new Thread(this, "Thread[" + num + "]").start();
     }
 
+    //First lets the user know the app is running
+    //The thread will then try to acquire the mutex.
     public void run() {
         System.out.println(Thread.currentThread().getName() + " Running");
+        //Then, lets the user know it's acquired.
+        //Thread will delay or hold onto the mutex for 1 second before release.
         try {
             while (!m.acquire())
                 continue;
             System.out.println(Thread.currentThread().getName() + " Acquired");
             delay(1000);
-        } finally {
+        }//Ensures teh mutex is always released, regardless of exception
+        //This will allow the next waiting thread to proceed.
+        finally {
             System.out.println(Thread.currentThread().getName() + " Released");
             m.release();
         }
     }
 
     private void delay(long timeInMs) {
+        //Ensures the time is at least a millisecond to not instantly return
         timeInMs = Math.max(1, timeInMs);
+        //Pauses for execution time
+        //If interrupted, ignore the exception
         try {
             Thread.sleep(timeInMs);
         } catch (InterruptedException ignored) {}
     }
+
 }
